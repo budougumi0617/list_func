283cb1441815b89fccc63efbde0710bfb596624b
budougumi0617 <budougumi0617@binc.jp>
2021-01-25 02:16:42 +0900

    :sparkles: list function/method

--- list_func/process.py
+++ list_func/process.py
@@ -1,7 +1,16 @@
-from ast import parse, dump
+from ast import parse, dump, ClassDef, FunctionDef
 
 
 def process(src: str) -> list[str]:
+    results: list[str] = []
     got = parse(src)
     print(dump(got, indent=4))
-    return []
+    for node in got.body:
+        if isinstance(node, FunctionDef):
+            results.append(node.name)
+        elif isinstance(node, ClassDef):
+            for cnode in node.body:
+                if isinstance(cnode, FunctionDef):
+                    results.append(f'{node.name}.{cnode.name}')
+
+    return results
