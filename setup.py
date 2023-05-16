9257f7be58d65542e81febf5f9cc1c6af22b6831
永野峻輔 <peace.infi0407@gmail.com>
2023-05-16 22:03:16 +0900

    chore: set entry point

--- setup.py
+++ setup.py
@@ -3,4 +3,9 @@ from setuptools import setup
 setup(
     name="list_func",
     version="0.0.1",
+    entry_points={
+        "console_scripts": [
+            "list_func=list_func.main:main",
+        ]
+    },
 )
